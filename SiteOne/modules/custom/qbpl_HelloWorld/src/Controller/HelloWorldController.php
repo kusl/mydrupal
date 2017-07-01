9e5d76a28038a1191e95f56c1b2844e3c93e7b38
Kushal Hada <kushaldeveloper@gmail.com>
2017-07-01 11:40:24 -0400

    add controller and router
    Signed-off-by: Kushal Hada <kushaldeveloper@gmail.com>

---
+++ SiteOne/modules/custom/qbpl_HelloWorld/src/Controller/HelloWorldController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 7/1/17
+ * Time: 11:19 AM
+ */
+namespace Drupal\qbpl_HelloWorld\Controller;
+class HelloWorldController extends \Drupal\Core\Controller\ControllerBase {
+    public function display(){
+        return array();
+    }
+}
